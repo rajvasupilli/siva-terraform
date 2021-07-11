301ead4849a38a49be68d8eb0e9c8b9afe95f9b8
root <root@ip-172-31-25-254.ec2.internal>
2021-07-11 17:57:07 +0000

    testing

--- provider.tf
+++ provider.tf
@@ -1,7 +1,6 @@
 # Configure the AWS Provider
 provider "aws" {
-  
-region = "us-east-1"
+  region = "us-east-1"
   access_key = "AKIAYAFLFRDBM34JHRGV"
   secret_key = "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"
 }
